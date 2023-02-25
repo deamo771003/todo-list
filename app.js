9b3ac3888c3e1684463dc4fe8de8a4e3a4c7e947
JimmyLin <disillusioned2077@gmail.com>
2023-02-25 23:50:56 +0800

    feat: debug Mongoose conntect

--- app.js
+++ app.js
@@ -1,19 +1,14 @@
-// app.js
 const express = require('express')
 const mongoose = require('mongoose') // 載入 mongoose
-
+// 加入這段 code, 僅在非正式環境時, 使用 dotenv
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 const app = express()
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }) // 設定連線到 mongoDB
 
-mongoose.connect('mongodb+srv://deamo771003:773001love@jimmylin.mg8evxp.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true }) // 設定連線到 mongoDB
 // 取得資料庫連線狀態
 const db = mongoose.connection
-
-
-// // 加入這段 code, 僅在非正式環境時, 使用 dotenv
-// if (process.env.NODE_ENV !== 'production') {
-//   require('dotenv').config()
-// }
-
 // 連線異常
 db.on('error', () => {
   console.log('mongodb error!')
@@ -24,9 +19,9 @@ db.once('open', () => {
 })
 
 app.get('/', (req, res) => {
-  res.send('hollow world3')
+  res.send('hello world!')
 })
 
 app.listen(3000, () => {
-  console.log('app is running on port 3000.')
+  console.log('App is running on http://localhost:3000')
 })
