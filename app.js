3e8e176d1967950da50a7e72d45d2efb29356a21
JimmyLin <disillusioned2077@gmail.com>
2023-03-02 21:58:34 +0800

    feat: add create todo function

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const mongoose = require('mongoose') // 載入 mongoose
 
+const bodyParser = require('body-parser')
+
 const Todo = require('./models/todo')
 
 const exphbs = require('express-handlebars')
@@ -12,7 +14,7 @@ if (process.env.NODE_ENV !== 'production') {
 const app = express()
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }) // 設定連線到 mongoDB
 
-// 取得資料庫連線狀態
+// 取得資料庫連線狀態 
 const db = mongoose.connection
 // 連線異常
 db.on('error', () => {
@@ -23,8 +25,11 @@ db.once('open', () => {
   console.log('mongodb connected!')
 })
 
+// 用 app.use 規定每一筆請求都需要透過 body-parser 進行前置處理
+app.use(bodyParser.urlencoded({ extended: true }))
+
 // 啟動hbs
-app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' })) // app.engine('指定副檔名', 模板 ({ main通用模板檔案, extname要被渲染的副檔名 }))
 app.set('view engine', 'hbs')
 
 app.get('/', (req, res) => {
@@ -33,6 +38,19 @@ app.get('/', (req, res) => {
     .lean() // 未經處裡的乾淨資料
     .then(todos => res.render('index', { todos })) // 然後...將todos資訊傳入index畫面
     .catch(error => console.log(error)) // 抓取到錯誤則顯示錯誤資訊
+})
+
+// 建立/todos/new路由
+app.get('/todos/new', (req, res) => {
+  return res.render('new')
+})
+
+// 設定一條新的路由，來接住表單資料，並且把資料送往資料庫。這個步驟就是 CRUD 裡的 Create 動作
+app.post('/todos', (req, res) => {
+  const name = req.body.name // 從 req.body 拿出表單裡的 name 資料
+  return Todo.create({ name }) // 存入資料庫
+    .then(() => res.redirect('/')) // 新增完成後導回首頁
+    .catch(error => console.log(error))
 })
 
 app.listen(3000, () => {
