7fa95700112822798f6affff501dd589530acf5d
JimmyLin <disillusioned2077@gmail.com>
2023-03-03 17:09:16 +0800

    feat: add edit function

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const bodyParser = require('body-parser')
 const Todo = require('./models/todo')
 
 const exphbs = require('express-handlebars')
+const todo = require('./models/todo')
 
 // 加入這段 code, 僅在非正式環境時, 使用 dotenv
 if (process.env.NODE_ENV !== 'production') {
@@ -36,7 +37,7 @@ app.get('/', (req, res) => {
   // take all Todo data
   Todo.find() // 抓所有資料
     .lean() // 未經處裡的乾淨資料
-    .then(todos => res.render('index', { todos })) // 然後...將todos資訊傳入index畫面
+    .then(todos => res.render('index', { todos })) // 然後...以todos為資訊傳入index畫面
     .catch(error => console.log(error)) // 抓取到錯誤則顯示錯誤資訊
 })
 
@@ -51,6 +52,38 @@ app.post('/todos', (req, res) => {
   return Todo.create({ name }) // 存入資料庫
     .then(() => res.redirect('/')) // 新增完成後導回首頁
     .catch(error => console.log(error))
+})
+
+// detail 路由
+app.get('/todos/:id', (req, res) => {
+  const id = req.params.id // params.id就是抓以上網址:id的資料參數
+  return Todo.findById(id) // 用網址參數id到Todo資料庫內找與mongoose流水ID相符的資料
+    .lean()
+    .then(todo => res.render('detail', { todo })) // 使用detail文檔加入todo數據
+    .catch(error => console.log(error))
+})
+
+// edit 路由
+app.get('/todos/:id/edit', (req, res) => {
+  const id = req.params.id // params.id就是抓以上網址:id的資料參數
+  return Todo.findById(id) // 用網址參數id到Todo資料庫內找與mongoose流水ID相符的資料
+    .lean()
+    .then(todo => res.render('edit', { todo })) // 使用detail文檔加入todo數據
+    .catch(error => console.log(error))
+})
+
+// edit 存取使用者資料
+app.post('/todos/:id/edit', (req, res) => {
+  const id = req.params.id
+  const name = req.body.name // name = 使用者key的name
+  return Todo.findById(id)
+    .then(todo => {
+      todo.name = name // 覆蓋原本todo.name
+      return todo.save() // ??
+    })
+    .then(() => res.redirect(`/todos/${id}`)) // ??
+    .catch(error => console.log(error))
+
 })
 
 app.listen(3000, () => {
