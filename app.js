8a4937933bf7cf82161f4f27894cce9f6986a4e3
JimmyLin <disillusioned2077@gmail.com>
2023-03-12 22:34:22 +0800

    refactor: modify mongoos config

--- app.js
+++ app.js
@@ -1,39 +1,34 @@
 const express = require('express')
-const mongoose = require('mongoose') // 載入 mongoose
-
 const bodyParser = require('body-parser')
-
 const methodOverride = require('method-override')
-
-const Todo = require('./models/todo')
+const exphbs = require('express-handlebars')
 
 const routes = require('./routes')
 
-const exphbs = require('express-handlebars')
-// const todo = require('./models/todo') // 多key，註解後測試完整性
-const { request } = require('express')
+// const { request } = require('express') // ??
 
 // 加入這段 code, 僅在非正式環境時, 使用 dotenv
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
 const app = express()
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }) // 設定連線到 mongoDB
 
-// 取得資料庫連線狀態 
-const db = mongoose.connection
-// 連線異常
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-// 連線成功
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
+require('./config/mongoose') // 載入mongoose資料 
+// 以下移至mongoose集中管理
+// mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }) // 設定連線到 mongoDB
+// // 取得資料庫連線狀態 
+// const db = mongoose.connection
+// // 連線異常
+// db.on('error', () => {
+//   console.log('mongodb error!')
+// })
+// // 連線成功
+// db.once('open', () => {
+//   console.log('mongodb connected!')
+// })
 
 // 用 app.use 規定每一筆請求都需要透過 body-parser 進行前置處理
 app.use(bodyParser.urlencoded({ extended: true }))
-
 // 啟用並定義methodOverride
 app.use(methodOverride('_method'))// <form>action路由後方加入?_method=XXX就可修改method屬性(HTTP預設只能POST&GET)
 
@@ -43,6 +38,7 @@ app.set('view engine', 'hbs')
 
 app.use(routes)
 // 以下分至routes集中管理
+// const Todo = require('./models/todo')
 // app.get('/', (req, res) => {
 //   // take all Todo data
 //   Todo.find() // 抓所有資料
--- models/seeds/todoSeeder.js
+++ models/seeds/todoSeeder.js
@@ -1,16 +1,8 @@
-const mongoose = require('mongoose')
 const Todo = require('../todo') // 載入model/todo
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
-}
 
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-const db = mongoose.connection
-db.on('error', () => {
-  console.log('mongodb error!')
-})
+const db = require('../../config/mongoose') // 呼叫mongoose裡的db
+
 db.once('open', () => {
-  console.log('mongodb connected!')
   for (let i = 0; i < 10; i++) {
     Todo.create({ name: `name-${i}` })
   }
