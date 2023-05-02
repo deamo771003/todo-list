d9bd805c1829ae037bd71bdf1d68b43eb68a7324
JimmyLin <disillusioned2077@gmail.com>
2023-05-02 17:28:22 +0800

    feat: add bcrypt to login route

--- config/passport.js
+++ config/passport.js
@@ -1,11 +1,13 @@
 const passport = require('passport')
 const LocalStrategy = require('passport-local').Strategy
 const User = require('../models/user')
+const bcrypt = require('bcryptjs')
 
 module.exports = app => {
   // 初始化passport模組
   app.use(passport.initialize())
   app.use(passport.session())
+
   // 設定本地登入策略
   passport.use(new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
     User.findOne({ email })
@@ -13,21 +15,25 @@ module.exports = app => {
         if (!user) {
           return done(null, false, { message: 'That email is not registered!' })
         }
-        if (user.password !== password) {
-          return done(null, false, { message: 'Email or Password incorrect!' })
-        }
-        return done(null, user)
+        // bcrypt朔回比對功能(使用者輸入的值, 資料庫裡的值)回傳布林值
+        return bcrypt.compare(password, user.password).then(isMach => { 
+          if (!isMach) {
+            return done(null, false, { message: 'Email or Password incorrect!' })
+          }
+          return done(null, user)
+        })
       })
       .catch(err => done(err, false))
   }))
+
   // 設定序列化與反序列化
   passport.serializeUser((user, done) => {
     done(null, user.id)
   })
   passport.deserializeUser((id, done) => {
     User.findById(id)
-    .lean()
-    .then(user => done(null,user))
-    .catch(err => done(err, null))
+      .lean()
+      .then(user => done(null, user))
+      .catch(err => done(err, null))
   })
 }
